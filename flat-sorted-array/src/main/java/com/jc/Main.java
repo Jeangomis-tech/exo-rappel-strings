747973f70972c37240d19c19497bc6e2a66e8178
Jean Gomis <jeangomistech2018@gmail.com>
2026-01-02 17:55:15 +0100

    Fusionner et trier un tableau

---
+++ flat-sorted-array/src/main/java/com/jc/Main.java
@@ -0,0 +1,23 @@
+package com.jc;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[] a = {1, 3, 5};
+        int[] b = {2, 4, 6};
+
+        int[] resultat = fusionner(a, b);
+
+        System.out.println(Arrays.toString(resultat));
+
+    }
+    public static int[] fusionner(int[] t1, int[] t2) {
+        return IntStream.concat(Arrays.stream(t1), Arrays.stream(t2))
+                .sorted()
+                .toArray();
+    }
+}
